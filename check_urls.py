025c2c5fe5616b1167299e80a8a6a1377eb4f396
rl1987 <rl1987@users.noreply.github.com>
2022-12-04 13:49:16 +0800

    Rework check_urls.py

--- check_urls.py
+++ check_urls.py
@@ -8,6 +8,9 @@ import requests
 PROXY_URL = "http://brd-customer-hl_cecd546c-zone-zone_residential_test-country-us:kncs4g88b3ca@zproxy.lum-superproxy.io:22225"
 
 def check_url(url):
+    if ".gov/" in url:
+        return True
+    
     headers = {
         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
         "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
@@ -40,6 +43,40 @@ def check_url(url):
 
     return False
 
+def check_urls_in_col(db, colname):
+    sql = "SELECT DISTINCT(`{}`) FROM `hospitals`;".format(colname)
+    print(sql)
+
+    res = db.sql(sql, result_format="json")
+
+    for row in res["rows"]:
+        try:
+            orig_urls = row.get(list(row.keys())[0])
+        except:
+            print(row)
+            continue
+        if orig_urls is None:
+            continue
+
+        orig_urls = orig_urls.split("|")
+        valid_urls = []
+
+        for url in orig_urls:
+            if check_url(url):
+                valid_urls.append(url)
+        
+        if orig_urls != valid_urls:
+            orig_urls = "|".join(orig_urls)
+            valid_urls = "|".join(valid_urls)
+            
+            if len(valid_urls) == 0:
+                sql = "UPDATE `hospitals` SET `{}` = NULL WHERE `{}` = \"{}\";".format(colname, colname, orig_urls)
+            else:
+                sql = "UPDATE `hospitals` SET `{}` = \"{}\" WHERE `{}` = \"{}\";".format(colname, valid_urls, colname, orig_urls)
+
+            print(sql)
+
+            db.sql(sql, result_format="json")
 
 def main():
     if len(sys.argv) != 2:
@@ -50,65 +87,10 @@ def main():
     dolt_db_dir = sys.argv[1]
 
     db = dolt.Dolt(dolt_db_dir)
-
-    sql = "SELECT * FROM `hospitals` WHERE `homepage` IS NOT NULL OR `cdm_url` IS NOT NULL OR `cdm_indirect_url` IS NOT NULL;"
-    print(sql)
-
-    res = db.sql(sql, result_format="json")
-
-    bad_homepages = set()
-    bad_direct_urls = set()
-    bad_indirect_urls = set()
-
-    for row in res["rows"]:
-        print(row)
-
-        homepage = row.get("homepage")
-        cdm_url = row.get("cdm_url")
-        cdm_indirect_url = row.get("cdm_indirect_url")
-
-        if homepage is not None:
-            if not check_url(homepage):
-                bad_homepages.add(homepage)
-
-        if cdm_url is not None:
-            if not check_url(cdm_url):
-                bad_direct_urls.add(cdm_url)
-
-        if cdm_indirect_url is not None:
-            if not check_url(cdm_indirect_url):
-                bad_indirect_urls.add(cdm_indirect_url)
-
-    for url in bad_homepages:
-        sql = 'UPDATE `hospitals` SET `homepage` = NULL WHERE `homepage` = "{}"'.format(
-            url
-        )
-        print(sql)
-        try:
-            db.sql(sql, result_format="json")
-        except Exception as e:
-            print(e)
-
-    for url in bad_direct_urls:
-        sql = 'UPDATE `hospitals` SET `cdm_url` = NULL WHERE `cdm_url` = "{}"'.format(
-            url
-        )
-        print(sql)
-        try:
-            db.sql(sql, result_format="json")
-        except Exception as e:
-            print(e)
-
-    for url in bad_indirect_urls:
-        sql = 'UPDATE `hospitals` SET `cdm_indirect_url` = NULL WHERE `cdm_indirect_url` = "{}"'.format(
-            url
-        )
-        print(sql)
-        try:
-            db.sql(sql, result_format="json")
-        except Exception as e:
-            print(e)
-
+    
+    check_urls_in_col(db, 'homepage')
+    check_urls_in_col(db, 'cdm_url')
+    check_urls_in_col(db, 'cdm_indirect_url')
 
 if __name__ == "__main__":
     main()
