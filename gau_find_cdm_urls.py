f444f7307cde3979b417ad77983c31e67fc3debb
rl1987 <rl1987@users.noreply.github.com>
2022-12-06 20:25:43 +0800

    Try reworking gau_find_cdm_urls.py with Playwright

--- gau_find_cdm_urls.py
+++ gau_find_cdm_urls.py
@@ -5,13 +5,14 @@ import sys
 from urllib.parse import urlparse, urljoin
 
 import doltcli as dolt
-import requests
 from lxml import html
+from playwright.sync_api import Playwright, sync_playwright
 
-PROXY_URL = "http://brd-customer-hl_cecd546c-zone-zone_residential_test-country-us:kncs4g88b3ca@zproxy.lum-superproxy.io:22225"
+PROXY_URL = "http://brd-customer-hl_cecd546c-zone-zone_dc_gau-country-us:oeodmyfnaa7r@zproxy.lum-superproxy.io:22225"
+
 
 def looks_like_pt_url(url):
-    look_for = [ "pricing", "price", "chargemaster", "billing", "transparency", "financial", "pay" ]
+    look_for = [ "pricing", "price", "chargemaster", "billing", "transparency", "financial", "pay"]
 
     for w in look_for:
         if w in url.lower():
@@ -20,7 +21,7 @@ def looks_like_pt_url(url):
     return False
 
 def looks_like_cdm_url(url):
-    look_for_words  = [ "cdm", "machine", "readable", "standard", "charges" ]
+    look_for_words  = [ "cdm", "machine", "readable", "standard", "charges", "master" ]
     look_for_ext = ["zip", "json", "csv", "xlsx", "xls", "pdf"]
 
     for w in look_for_words:
@@ -33,38 +34,34 @@ def looks_like_cdm_url(url):
 
     return False
 
-def scrape_direct_url(indirect_url):
-    proxies = {
-        "http": PROXY_URL,
-        "https": PROXY_URL
+def scrape_direct_url(browser, indirect_url):
+    print("Scraping:", indirect_url)
+    proxy = {
+        'server': 'http://zproxy.lum-superproxy.io:22225',
+        'username': 'brd-customer-hl_cecd546c-zone-zone_dc_gau-country-us',
+        'password': 'oeodmyfnaa7r'
     }
 
     try:
-        resp = requests.get(indirect_url, proxies=proxies, timeout=5.0)
-        print(resp.url)
-    except:
-        return
-    
-    if resp.status_code != 200:
-        return
-
-    try:
-        tree = html.fromstring(resp.text)
-    except:
+        page = browser.new_page(proxy=proxy)
+        page.goto(indirect_url)
+        tree = html.fromstring(page.content())
+    except Exception as e:
+        print(e)
         return
 
     for link in tree.xpath('//a/@href'):
-        url = urljoin(resp.url, indirect_url)
+        url = urljoin(page.url, link)
 
         if looks_like_cdm_url(url):
+            print("Direct URL:", url)
             return url
 
-def do_recon(db, homepage_url):
+def do_recon(db, browser, homepage_url):
     print("Checking:", homepage_url)
-    p = subprocess.run(["gau", "--mt", "text/html", "--threads", "8", "--providers", "wayback", homepage_url], 
+    p = subprocess.run(["gau", "--proxy", PROXY_URL,
+                        "--mt", "text/html", "--threads", "8", "--providers", "wayback", homepage_url], 
                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    print("!")
 
     for line in p.stdout.decode('utf-8').split('\n'):
         print(line)
@@ -75,7 +72,7 @@ def do_recon(db, homepage_url):
 
         if looks_like_pt_url(url):
             indirect_url = url
-            direct_url = scrape_direct_url(url)
+            direct_url = scrape_direct_url(browser, url)
 
             if direct_url is None:
                 continue
@@ -85,13 +82,16 @@ def do_recon(db, homepage_url):
             sql = 'UPDATE `hospitals` SET `cdm_url` = "{}", `cdm_indirect_url` = "{}" WHERE `homepage` = "{}" AND `cdm_url` IS NULL;'.format(direct_url, indirect_url, homepage_url)
             print(sql)
 
-            db.sql(sql)
+            db.sql(sql, result_format="json")
 
 def main():
     if len(sys.argv) != 2:
         print("Usage:")
         print("{} <dolt_db_dir>".format(sys.argv[0]))
         return
+
+    playwright = sync_playwright().start()
+    browser = playwright.chromium.launch()
 
     dolt_db_dir = sys.argv[1]
 
@@ -104,8 +104,25 @@ def main():
 
     for row in res["rows"]:
         homepage_url = row["homepage"]
+        
+        sql = "SELECT COUNT(*) FROM `hospitals` WHERE `homepage` = \"{}\";".format(homepage_url)
+        print(sql)
 
-        do_recon(db, homepage_url)
+        try:
+            res2 = db.sql(sql, result_format="json")
+            k = list(res2["rows"][0].keys())[0]
+            n = res2["rows"][0][k]
+            if n != 1:
+                continue
+        except Exception as e:
+            print(e)
+
+        print(homepage_url)
+
+        do_recon(db, browser, homepage_url)
+
+    browser.close()
+
 
 if __name__ == "__main__":
     main()
