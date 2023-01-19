ad6b6c55b61c67fbe7c273d857df5ef47e96940e
rl1987 <rl1987@users.noreply.github.com>
2023-01-19 20:06:01 +0800

    Install crontab-ui on the server

--- provision.sh
+++ provision.sh
@@ -18,7 +18,7 @@ mkdir /root/data
 
 pushd /root/data || exit
 dolt clone rl1987/paylesshealth
-popd /root/data || exit
+popd || exit
 
 wget https://github.com/lc/gau/releases/download/v2.1.2/gau_2.1.2_linux_amd64.tar.gz -O /tmp/gau_2.1.2_linux_amd64.tar.gz
 pushd /tmp || exit
@@ -32,3 +32,28 @@ chmod 0600 /swapfile
 mkswap /swapfile
 swapon /swapfile
 echo "/swapfile swap swap sw 0 0" >> /etc/fstab
+
+curl -fsSL https://deb.nodesource.com/setup_19.x -o /tmp/install_node.sh
+bash /tmp/install_node.sh
+apt-get install -y gcc g++ make nodejs
+
+npm install crontab-ui -g
+npm install pm2 -g
+
+{
+    echo "HOST=0.0.0.0"
+    echo "PORT=9000"
+    echo "BASIC_AUTH_USER=rl"
+    echo "BASIC_AUTH_PWD=r29483tuy3490y30f98h"
+    echo "ENABLE_AUTOSAVE=true"
+} >> /etc/environment
+
+export HOST=0.0.0.0
+export PORT=9000
+export BASIC_AUTH_USER=rl
+export BASIC_AUTH_PWD=r29483tuy3490y30f98h
+export ENABLE_AUTOSAVE=true
+
+pm2 start crontab-ui -- --autosave
+pm2 save
+pm2 startup systemd
