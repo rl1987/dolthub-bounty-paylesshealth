974d70ff05658a9983947617cc953dbdeabe2d8d
rl1987 <rl1987@users.noreply.github.com>
2023-03-30 16:59:17 +0700

    Start new Python script for a second idea

---
+++ crawl_for_stcharges.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+
+# TODO: implement basic crawling (a la hakrawler) to find standard charges
+# file URLs. Save the findings to CSV. Leave the hostpital-file matching
+# for later.
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
